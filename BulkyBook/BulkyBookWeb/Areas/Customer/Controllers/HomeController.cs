e053d199bd2d64af78585b8d8b818315630efd5e
yashraj031 <112865913+yashraj031@users.noreply.github.com>
2023-02-06 18:13:30 +0530

    End of Session 7

--- BulkyBook/BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBook/BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using BulkyBook.Models;
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.Diagnostics;
 
 namespace BulkyBookWeb.Areas.Customer.Controllers
@@ -8,18 +11,30 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitofwork _unitofwork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IUnitofwork unitofWork)
         {
             _logger = logger;
+            _unitofwork = unitofWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productsList = _unitofwork.Product.GetAll(includeProperties: "category,CoverType");
+            return View(productsList);
         }
 
-        public IActionResult Privacy()
+        public IActionResult Details(int id)
+        {
+            ShoppingCart cartObj = new()
+            {
+                Count = 1,
+                Product = _unitofwork.Product.GetFirstOrDefault(u => u.ID == id, includeProperties: "category,CoverType")
+            };     
+            return View(cartObj);
+		}
+		public IActionResult Privacy()
         {
             return View();
         }
